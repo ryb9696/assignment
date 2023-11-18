8a209033320df78175194640a3c45078c9efd497
Ubuntu <ubuntu@ip-172-31-80-110.ec2.internal>
2023-11-18 10:55:24 +0000

    prod stage

---
+++ prod.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Get the environment name from the Jenkins pipeline
+environment="prod"
+
+# Deploy the shell script to the target environment
+echo "Deploying shell script to $environment environment..."
+
+# TODO: Implement the deployment logic here
+
+# Print a success message
+echo "Deployed to $environment environment successfully!"
+
