b8c801ffbc0df95b74ea765e6315456d4f870899
ryb9696 <135820856+ryb9696@users.noreply.github.com>
2023-11-18 17:30:43 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Get the environment name from the Jenkins pipeline
+environment="dev"
+
+# Deploy the shell script to the target environment
+echo "Deploying shell script to $environment environment..."
+
+# TODO: Implement the deployment logic here
+
+# Print a success message
+echo "Deployed to $environment environment successfully!"
